5e2e0a4dd916e7d15fa3114ecbce79076e7f9162
Vy Huynh <huynhvy@mail.uc.edu>
2024-05-02 11:18:08 -0400

    axios services

--- packages/client/src/services/AssessmentService.js
+++ packages/client/src/services/AssessmentService.js
@@ -1,28 +1,38 @@
 import Axios from '../utils/http.config';
 
 export class AssessmentService {
-  static submit(assessment) {
+  static async submit(assessment) {
     try {
       // Choose the correct method, url, and data to send
       // in a request to the express packages/api/src/routes/assessment.js
       // NOTE: the http.config file automatically adds /api to the front of your url
-      return Axios.post(`/assessment/submit`, assessment)
-        .then((response) => response.data.data.assessment);
+      return await Axios.post(`/assessment`, { assessment })
+        .then(response => response.data);
     }
     catch (err) {
-      throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
+      } else if (err.request) {
+        // The request was made but no response was received
+        throw new Error(`No response was received from the server.`);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        throw new Error(`An error occurred while setting up the request.`);
+      }
     }
   }
 
-  static getList() {
+  static async getList() {
     try {
       // Choose the correct method, url, and data to send
       // in a request to the express packages/api/src/routes/assessment.js
       // NOTE: the http.config file automatically adds /api to the front of your url
-      return Axios.get(`/assessment/submit`, {
+      const response = await Axios.get(`/assessment`, {
         params: {},
-      })
-        .then(response => response.data.data.assessment);
+      });
+      return response.data.data.assessment;
     }
     catch (err) {
       throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
